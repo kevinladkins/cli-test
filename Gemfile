1244d14d02f64d958edabfd0de7fb5181fefb505
Kevin Adkins <kevinladkins@gmail.com>
2017-02-05 18:14:51 +0000

    built working model

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+gem nokogiri (1.6.7.1)
+
+gem pry (0.10.3)
+
+gem rspec (3.4.0)
+
 # gem "rails"
--- lib/scraper_test.rb
+++ lib/scraper_test.rb
@@ -1,6 +1,16 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
+
+
 class ScraperTest
 
-  def initialize
+
+  def self.get_data
+    doc = Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/"))
+    puts doc
   end
+
 
 end
